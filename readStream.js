9a2d1f413d3e8c7e9e56bcf15d4d8dd5749c8e07
sethdanny <naddulidaniel1994@gmail.com>
2023-03-17 23:50:06 +0300

    readstream

---
+++ readStream.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* create a read stream from a big 
+chunk of file */
+const fs = require('fs');
+const readStream = fs.createReadStream('./docs/blog3.txt', { encoding : 'utf8'});
+readStream.on('data', (chunk) => {
+    console.log(chunk);
+})
